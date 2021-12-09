4c5e1a8bd317ba2e198475be215084910d7f0127
Annonator <andreaspohl89@gmail.com>
2021-12-09 19:21:46 +0100

    Added first draft for Resolve #4

---
+++ src/PlayFabHelpers/Util/Repository/LocalMasterPlayerAccountRepository.cs
@@ -0,0 +1,39 @@
+﻿using PlayFabBuddy.PlayFabHelpers.Entities.Accounts;
+using System.Text.Json;
+
+namespace PlayFabBuddy.PlayFabHelpers.Util.Repository
+{
+    public class LocalMasterPlayerAccountRepository : IRepository<MasterPlayerAccountEntity>
+    {
+        private string configPath;
+        private List<MasterPlayerAccountEntity>? MasterAccountList;
+
+        public LocalMasterPlayerAccountRepository(string pathToConfig)
+        {
+            this.configPath = pathToConfig;
+        }
+
+        public List<MasterPlayerAccountEntity> Get()
+        {
+            string jsonString = File.ReadAllText(this.configPath);
+
+            var entityList = JsonSerializer.Deserialize<List<MasterPlayerAccountEntity>>(jsonString);
+
+            if (entityList == null)
+            {
+                throw new NullReferenceException($"Entity List, is null, Error on Deserialization");
+            }
+
+            return entityList;
+        }
+
+        public async void Save(List<MasterPlayerAccountEntity> toSave)
+        {
+            MasterAccountList = toSave;
+
+            using FileStream writeStream = File.Create(this.configPath);
+            await JsonSerializer.SerializeAsync<List<MasterPlayerAccountEntity>>(writeStream, toSave);
+            await writeStream.DisposeAsync();
+        }
+    }
+}
