c1f52fb4ec7911d1f0cef31d9df2cba639a13dc8
Annonator <andreaspohl89@gmail.com>
2022-01-21 19:08:32 +0100

    Added Specter.Console and added Creating / deleting user command

---
+++ src/PlayFabBuddy.Cli/Commands/Player/CreateNewPlayersCommand.cs
@@ -0,0 +1,49 @@
+﻿using PlayFabBuddy.Lib.Commands.Player;
+using PlayFabBuddy.Lib.Entities.Accounts;
+using PlayFabBuddy.Lib.Util.IoC;
+using PlayFabBuddy.Lib.Util.Repository;
+using Spectre.Console;
+using Spectre.Console.Cli;
+
+namespace PlayFabBuddy.Cli.Commands.Player;
+
+public class CreateNewPlayersCommand : AsyncCommand<CreateNewPlayersCommandSettings>
+{
+    private readonly IRepository<MasterPlayerAccountEntity> _repository;
+
+    public CreateNewPlayersCommand()
+    {
+        _repository = DependencyInjection.Instance.Resolve<IRepository<MasterPlayerAccountEntity>>();
+    }
+
+    public override async Task<int> ExecuteAsync(CommandContext context, CreateNewPlayersCommandSettings settings)
+    {
+        await AnsiConsole.Progress().StartAsync(async ctx =>
+        {
+            var task = ctx.AddTask("[yellow]Creating Users[/]", false);
+            task.StartTask();
+
+            await CreateUsers(settings.NumberOfUsers, task);
+
+            task.StopTask();
+        });
+
+        AnsiConsole.MarkupLine("[bold green]All Users Created![/]");
+
+        return 0;
+    }
+
+    private async Task CreateUsers(int concurrentUsers, ProgressTask task)
+    {
+        var commandList = new List<Task<MasterPlayerAccountEntity>>();
+        for (var i = 0; i < concurrentUsers; i++)
+        {
+            task.Increment(i % 10);
+            commandList.Add(new RegisterNewPlayerCommand().ExecuteAsync());
+        }
+
+        var results = await Task.WhenAll(commandList);
+
+        await _repository.Append(results.ToList());
+    }
+}
--- src/PlayFabBuddy.Lib/Util/Repository/LocalMasterPlayerAccountRepository.cs
+++ src/PlayFabBuddy.Lib/Util/Repository/LocalMasterPlayerAccountRepository.cs
@@ -1,18 +1,18 @@
-﻿using System.Text.Json;
+﻿using PlayFabBuddy.Lib.Entities.Accounts;
+using System.Text.Json;
 using System.Text.Json.Serialization;
-using PlayFabBuddy.Lib.Entities.Accounts;
 
 namespace PlayFabBuddy.Lib.Util.Repository;
 
 public class LocalMasterPlayerAccountRepository : IRepository<MasterPlayerAccountEntity>
 {
-    private readonly string configPath;
-    private readonly JsonSerializerOptions jsonOptions;
+    private readonly string _configPath;
+    private readonly JsonSerializerOptions _jsonOptions;
 
     public LocalMasterPlayerAccountRepository(string pathToConfig)
     {
-        configPath = pathToConfig;
-        jsonOptions = new JsonSerializerOptions
+        _configPath = pathToConfig;
+        _jsonOptions = new JsonSerializerOptions
         {
             ReferenceHandler = ReferenceHandler.Preserve,
             WriteIndented = true
@@ -21,19 +21,31 @@ public class LocalMasterPlayerAccountRepository : IRepository<MasterPlayerAccoun
 
     public List<MasterPlayerAccountEntity> Get()
     {
-        var jsonString = File.ReadAllText(configPath);
+        var jsonString = File.ReadAllText(_configPath);
 
-        var entityList = JsonSerializer.Deserialize<List<MasterPlayerAccountEntity>>(jsonString, jsonOptions);
+        var entityList = JsonSerializer.Deserialize<List<MasterPlayerAccountEntity>>(jsonString, _jsonOptions);
 
-        if (entityList == null) throw new NullReferenceException("Entity List, is null, Error on Deserialization");
+        if (entityList == null)
+        {
+            throw new NullReferenceException("Entity List, is null, Error on Deserialization");
+        }
 
         return entityList;
     }
 
     public async Task Save(List<MasterPlayerAccountEntity> toSave)
     {
-        using var writeStream = File.Create(configPath);
-        await JsonSerializer.SerializeAsync(writeStream, toSave, jsonOptions);
+        await using var writeStream = File.Create(_configPath);
+        await JsonSerializer.SerializeAsync(writeStream, toSave, _jsonOptions);
+        await writeStream.DisposeAsync();
+    }
+
+    public async Task Append(List<MasterPlayerAccountEntity> toAppend)
+    {
+        var oldUsers = Get();
+        oldUsers.AddRange(toAppend);
+        await using var writeStream = File.Create(_configPath);
+        await JsonSerializer.SerializeAsync(writeStream, oldUsers, _jsonOptions);
         await writeStream.DisposeAsync();
     }
 }
