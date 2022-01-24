8230e368e7ff844ec7cc50b5e233291afcc76dcd
Annonator <andreaspohl89@gmail.com>
2022-01-24 12:54:32 +0100

    Fixed Folder structure

---
+++ src/PlayFabBuddy.Tests/PlayFabBuddy.InfrastructureTests/IoC/DependencyInjectionTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PlayFabBuddy.Infrastructure.IoC;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayFabBuddy.Infrastructure.IoC.Tests
+{
+    [TestClass()]
+    public class DependencyInjectionTests
+    {
+        [TestMethod()]
+        public void RegisterTest()
+        {
+        }
+
+        [TestMethod()]
+        public void ResolveTest()
+        {
+        }
+
+        [TestMethod()]
+        public void ResolveTest1()
+        {
+        }
+    }
+}
