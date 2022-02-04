a2edcab06670f00db9c50be0a6c5159701c7454c
Johannes Ebner <Structed@users.noreply.github.com>
2022-02-04 16:06:12 +0100

    Added CLI command, but WIP (not yet calling infra command)

---
+++ src/PlayFabBuddy.Cli/Commands/Player/DeletePlayersBySegmentCommand.cs
@@ -0,0 +1,24 @@
+﻿using Spectre.Console;
+using Spectre.Console.Cli;
+
+namespace PlayFabBuddy.Cli.Commands.Player;
+
+public class DeletePlayersBySegmentCommand : AsyncCommand<DeletePlayersBySegmentCommandSettings>
+{
+    public async override Task<int> ExecuteAsync(CommandContext context, DeletePlayersBySegmentCommandSettings settings)
+    {
+        await AnsiConsole.Progress().StartAsync(async ctx =>
+        {
+            var task = ctx.AddTask("[yellow]Deleting Players[/]", false);
+            task.StartTask();
+
+            // TODO call the command
+            
+            task.StopTask();
+        });
+
+        AnsiConsole.MarkupLine("[bold green]All Players deleted![/]");
+
+        return 0;
+    }
+}
---
+++ src/PlayFabBuddy.Cli/Commands/Player/DeletePlayersBySegmentCommandSettings.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel;
+using Spectre.Console.Cli;
+
+namespace PlayFabBuddy.Cli.Commands.Player;
+
+public class DeletePlayersBySegmentCommandSettings : PlayerSettings
+{
+    [Description(
+        $"The Segment Name the player to delete are in. Use {Lib.Commands.Player.DeletePlayersBySegmentCommand.AllPlayersSegmentName} to telete all Players.")]
+    [CommandArgument(0, "<SegmentName>")]
+    public string SegmentName { get; init; } = "";
+}
