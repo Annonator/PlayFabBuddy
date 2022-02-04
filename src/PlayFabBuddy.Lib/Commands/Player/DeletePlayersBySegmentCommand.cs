dc90784d3c28eb4fb5cdd8845377b50bdd497c03
Johannes Ebner <Structed@users.noreply.github.com>
2022-02-04 15:04:52 +0100

    Implemented deletion of players

--- src/PlayFabBuddy.Lib/Commands/Player/DeletePlayersBySegmentCommand.cs
+++ src/PlayFabBuddy.Lib/Commands/Player/DeletePlayersBySegmentCommand.cs
@@ -14,15 +14,45 @@ public class DeletePlayersBySegmentCommand
     {
         var allPlayersSegmentId = await GetAllPlayersSegmentId();
         var playersInSegment = await GetPlayersInSegment(allPlayersSegmentId);
-        
-        playersInSegment.Result.PlayerProfiles.ForEach(player =>
-        {
-            // TODO: delete player
-        });
-        
+
+        await DeletePlayers(playersInSegment);
+
         return await Task.FromResult(true);
     }
 
+    /// <summary>
+    /// Deletes a list of players
+    /// </summary>
+    /// <param name="players"></param>
+    private async static Task DeletePlayers(PlayFabResult<GetPlayersInSegmentResult> players)
+    {
+        var tasks = new List<Task>();
+        foreach (var playerProfile in players.Result.PlayerProfiles)
+        {
+            tasks.Add(DeleteTitlePLayer(playerProfile));
+        }
+
+        await Task.WhenAll(tasks);
+    }
+
+    /// <summary>
+    /// Delete a title player
+    /// </summary>
+    /// <param name="player"></param>
+    private async static Task DeleteTitlePLayer(PlayerProfile player)
+    {
+        var deletePlayerRequest = new DeletePlayerRequest
+        {
+            PlayFabId = player.PlayerId
+        };
+        var result = await PlayFabAdminAPI.DeletePlayerAsync(deletePlayerRequest);
+    }
+
+    /// <summary>
+    /// Get all players in a given segment, identified by <paramref name="segmentId"/>
+    /// </summary>
+    /// <param name="segmentId">The segment's ID</param>
+    /// <returns>Al the players in the segment</returns>
     private async static Task<PlayFabResult<GetPlayersInSegmentResult>> GetPlayersInSegment(string segmentId)
     {
         var getPlayersInSegmentRequest = new GetPlayersInSegmentRequest
