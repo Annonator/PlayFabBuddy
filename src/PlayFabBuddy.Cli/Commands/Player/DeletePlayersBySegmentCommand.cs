04e053a958b09b93a8fa909e1262f15bfad09b8c
Johannes Ebner <Structed@users.noreply.github.com>
2022-02-05 22:52:16 +0100

    Calling the actual command for deleting all players of a given segment

--- src/PlayFabBuddy.Cli/Commands/Player/DeletePlayersBySegmentCommand.cs
+++ src/PlayFabBuddy.Cli/Commands/Player/DeletePlayersBySegmentCommand.cs
@@ -1,10 +1,20 @@
-﻿using Spectre.Console;
+﻿using PlayFabBuddy.Lib.Interfaces.Adapter;
+using Spectre.Console;
 using Spectre.Console.Cli;
 
 namespace PlayFabBuddy.Cli.Commands.Player;
 
 public class DeletePlayersBySegmentCommand : AsyncCommand<DeletePlayersBySegmentCommandSettings>
 {
+    private readonly IPlayStreamAdapter playStreamAdapter;
+    private readonly IPlayerAccountAdapter playerAccountAdapter;
+
+    public DeletePlayersBySegmentCommand(IPlayStreamAdapter playStreamAdapter, IPlayerAccountAdapter playerAccountAdapter)
+    {
+        this.playStreamAdapter = playStreamAdapter;
+        this.playerAccountAdapter = playerAccountAdapter;
+    }
+
     public async override Task<int> ExecuteAsync(CommandContext context, DeletePlayersBySegmentCommandSettings settings)
     {
         await AnsiConsole.Progress().StartAsync(async ctx =>
@@ -12,7 +22,8 @@ public class DeletePlayersBySegmentCommand : AsyncCommand<DeletePlayersBySegment
             var task = ctx.AddTask("[yellow]Deleting Players[/]", false);
             task.StartTask();
 
-            // TODO call the command
+            var command = new Lib.Commands.Player.DeletePlayersBySegmentCommand(this.playStreamAdapter, this.playerAccountAdapter);
+            await command.ExecuteAsync(settings.SegmentName);
             
             task.StopTask();
         });
