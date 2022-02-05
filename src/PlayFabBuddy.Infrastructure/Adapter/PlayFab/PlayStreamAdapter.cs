47955b625c12a8d5e41b8595c5378d07ab11469f
Johannes Ebner <Structed@users.noreply.github.com>
2022-02-05 21:50:43 +0100

    Moved appropriate methods from DeletePlayersBySegmentCommand to PlayStreamAdapter

--- src/PlayFabBuddy.Infrastructure/Adapter/PlayFab/PlayStreamAdapter.cs
+++ src/PlayFabBuddy.Infrastructure/Adapter/PlayFab/PlayStreamAdapter.cs
@@ -1,8 +1,49 @@
-﻿using PlayFabBuddy.Lib.Interfaces.Adapter;
+﻿using PlayFab;
+using PlayFab.AdminModels;
+using PlayFabBuddy.Lib.Interfaces.Adapter;
 
 namespace PlayFabBuddy.Infrastructure.Adapter.PlayFab;
 
 public class PlayStreamAdapter: IPlayStreamAdapter
 {
-    
+    /// <summary>
+    /// Get all players in a given segment, identified by <paramref name="segmentId"/>
+    /// </summary>
+    /// <param name="segmentId">The segment's ID</param>
+    /// <returns>Al the players in the segment</returns>
+    private async static Task<PlayFabResult<GetPlayersInSegmentResult>> GetPlayersInSegment(string segmentId)
+    {
+        var getPlayersInSegmentRequest = new GetPlayersInSegmentRequest
+        {
+            SegmentId = segmentId
+        };
+        var playersInSegment = await PlayFabAdminAPI.GetPlayersInSegmentAsync(getPlayersInSegmentRequest);
+        return playersInSegment;
+    }
+
+    /// <summary>
+    /// Fetches the ID of the "All Players" segment for the Title
+    /// TODO: Good method name???
+    /// </summary>
+    /// <returns>The SegmentId</returns>
+    /// <exception cref="Exception">When the segment could not be found</exception>
+    private async static Task<string> GetAllPlayersSegmentId()
+    {
+        var allSegmentsResponse = await PlayFabAdminAPI.GetAllSegmentsAsync(new GetAllSegmentsRequest());
+        var allPlayersSegmentId = "";
+        foreach (var segmentResult in allSegmentsResponse.Result.Segments)
+        {
+            if (segmentResult.Name == IPlayStreamAdapter.AllPlayersSegmentName)
+            {
+                allPlayersSegmentId = segmentResult.Id;
+            }
+        }
+
+        if (string.IsNullOrWhiteSpace(allPlayersSegmentId))
+        {
+            throw new Exception("All Players Segment was not found");
+        }
+
+        return allPlayersSegmentId;
+    }
 }
--- src/PlayFabBuddy.Lib/Commands/Player/DeletePlayersBySegmentCommand.cs
+++ src/PlayFabBuddy.Lib/Commands/Player/DeletePlayersBySegmentCommand.cs
@@ -45,45 +45,4 @@ public class DeletePlayersBySegmentCommand
         };
         var result = await PlayFabAdminAPI.DeletePlayerAsync(deletePlayerRequest);
     }
-
-    /// <summary>
-    /// Get all players in a given segment, identified by <paramref name="segmentId"/>
-    /// </summary>
-    /// <param name="segmentId">The segment's ID</param>
-    /// <returns>Al the players in the segment</returns>
-    private async static Task<PlayFabResult<GetPlayersInSegmentResult>> GetPlayersInSegment(string segmentId)
-    {
-        var getPlayersInSegmentRequest = new GetPlayersInSegmentRequest
-        {
-            SegmentId = segmentId
-        };
-        var playersInSegment = await PlayFabAdminAPI.GetPlayersInSegmentAsync(getPlayersInSegmentRequest);
-        return playersInSegment;
-    }
-
-    /// <summary>
-    /// Fetches the ID of the "All Players" segment for the Title
-    /// TODO: Good method name???
-    /// </summary>
-    /// <returns>The SegmentId</returns>
-    /// <exception cref="Exception">When the segment could not be found</exception>
-    private async static Task<string> GetAllPlayersSegmentId()
-    {
-        var allSegmentsResponse = await PlayFabAdminAPI.GetAllSegmentsAsync(new GetAllSegmentsRequest());
-        var allPlayersSegmentId = "";
-        foreach (var segmentResult in allSegmentsResponse.Result.Segments)
-        {
-            if (segmentResult.Name == AllPlayersSegmentName)
-            {
-                allPlayersSegmentId = segmentResult.Id;
-            }
-        }
-
-        if (string.IsNullOrWhiteSpace(allPlayersSegmentId))
-        {
-            throw new Exception("All Players Segment was not found");
-        }
-
-        return allPlayersSegmentId;
-    }
 }
