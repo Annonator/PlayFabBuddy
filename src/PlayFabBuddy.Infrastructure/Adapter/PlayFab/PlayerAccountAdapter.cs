14ec7517be0c6c7207c1b1d3190d5400aa8084b5
Johannes Ebner <Structed@users.noreply.github.com>
2022-02-06 17:06:54 +0100

    Implemented check if Master Player Account has multiple titles

--- src/PlayFabBuddy.Infrastructure/Adapter/PlayFab/PlayerAccountAdapter.cs
+++ src/PlayFabBuddy.Infrastructure/Adapter/PlayFab/PlayerAccountAdapter.cs
@@ -11,12 +11,26 @@ public class PlayerAccountAdapter : IPlayerAccountAdapter
 {
     public async Task Delete(MasterPlayerAccountEntity account)
     {
+        var playedTitleList = await GetPlayedTitleList(account);
+        if (playedTitleList.MoreThanOne())
+        {
+            throw new Exception($"Master PlayerAccount ID \"{account.Id}\" has more than one Title");
+        }
+        
         var request = new DeleteMasterPlayerAccountRequest
         {
             PlayFabId = account.Id
         };
 
         await PlayFabAdminAPI.DeleteMasterPlayerAccountAsync(request);
+    }
+
+    public async Task<PlayedTitlesListEntity> GetPlayedTitleList(MasterPlayerAccountEntity account)
+    {
+        var request = new GetPlayedTitleListRequest { PlayFabId = account.Id };
+
+        var response =await PlayFabAdminAPI.GetPlayedTitleListAsync(request);
+        return new PlayedTitlesListEntity(response.Result.TitleIds.ToArray());
     }
 
     public async Task<MasterPlayerAccountEntity> LoginWithCustomId(string customId)
---
+++ src/PlayFabBuddy.Lib/Entities/Accounts/PlayedTitlesListEntity.cs
@@ -0,0 +1,20 @@
+﻿namespace PlayFabBuddy.Lib.Entities.Accounts;
+
+public class PlayedTitlesListEntity
+{
+    public string[] TitleIds { get; }
+    
+    public PlayedTitlesListEntity(string[] titleIds)
+    {
+        this.TitleIds = titleIds;
+    }
+
+    /// <summary>
+    /// returns whether there are more than one title in the list
+    /// </summary>
+    /// <returns>Whether there are more than one title in the list</returns>
+    public bool MoreThanOne()
+    {
+        return this.TitleIds.Length > 1;
+    }
+}
