ea1090f14eb042fc1bf01fc05f93b466b7dd623a
Johannes Ebner <Structed@users.noreply.github.com>
2022-02-06 20:40:21 +0100

    Added constants for pre-defined segments

--- src/PlayFabBuddy.Lib/Interfaces/Adapter/IPlayStreamAdapter.cs
+++ src/PlayFabBuddy.Lib/Interfaces/Adapter/IPlayStreamAdapter.cs
@@ -4,7 +4,25 @@ namespace PlayFabBuddy.Lib.Interfaces.Adapter;
 
 public interface IPlayStreamAdapter
 {
+    /// <summary>
+    /// All Players
+    /// </summary>
     public const string AllPlayersSegmentName = "All Players";
+    
+    /// <summary>
+    /// Last login greater than 30 days ago
+    /// </summary>
+    public const string LapsedPlayersSegmentName = "Lapsed Players";
+    
+    /// <summary>
+    /// Total value to date in USD greater than $0.00
+    /// </summary>
+    public const string PayersSegmentName = "Payers";
+    
+    /// <summary>
+    /// First login less than 14 days ago AND First login greater than or equal to 7 days ago AND Last login less than 7 days ago
+    /// </summary>
+    public const string WeekTwoActivePlayersSegmentName = "Week Two Active Players";
 
     /// <summary>
     /// Get all players in a given segment, identified by <paramref name="segmentId"/>
