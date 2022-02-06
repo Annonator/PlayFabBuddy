5ee915470f3ddf938c93641c3bae53dde2171d7f
Johannes Ebner <Structed@users.noreply.github.com>
2022-02-06 22:31:09 +0100

    Fix typos

--- src/PlayFabBuddy.Lib/Entities/Accounts/PlayedTitlesListEntity.cs
+++ src/PlayFabBuddy.Lib/Entities/Accounts/PlayedTitlesListEntity.cs
@@ -1,12 +1,12 @@
 ﻿namespace PlayFabBuddy.Lib.Entities.Accounts;
 
 /// <summary>
-/// Represents a list of tiles an Account has Played in
+/// Represents a list of Tiles an Account has played in
 /// </summary>
 public class PlayedTitlesListEntity
 {
     /// <summary>
-    /// AA list of PlayFab Title IDs
+    /// A list of PlayFab Title IDs
     /// </summary>
     public string[] TitleIds { get; }
     
@@ -16,9 +16,9 @@ public class PlayedTitlesListEntity
     }
 
     /// <summary>
-    /// returns whether there are more than one title in the list
+    /// Returns whether there is more than one Title in the list
     /// </summary>
-    /// <returns>Whether there are more than one title in the list</returns>
+    /// <returns>Whether there is more than one Title in the list</returns>
     public bool MoreThanOne()
     {
         return this.TitleIds.Length > 1;
