4237527b11253f91fc294b6fbd5d0e05e1888bb1
Andreas Pohl <andreaspohl89@gmail.com>
2022-02-13 14:51:24 +0100

    Started refactoring

---
+++ src/PlayFabBuddy.Cli/Commands/Player/DeletePlayersCommand.cs
@@ -0,0 +1,69 @@
+﻿using PlayFabBuddy.Lib.Aggregate;
+using PlayFabBuddy.Lib.Interfaces.Adapter;
+using PlayFabBuddy.Lib.Interfaces.Repositories;
+using PlayFabBuddy.Lib.UseCases.Player;
+using Spectre.Console;
+using Spectre.Console.Cli;
+
+namespace PlayFabBuddy.Cli.Commands.Player;
+
+public class DeletePlayersCommand : AsyncCommand<DeletePlayersCommandSettings>
+{
+    private readonly IRepository<MasterPlayerAccountAggregate> _repository;
+    private readonly IPlayerAccountAdapter _playerAccountAdapter;
+
+    public DeletePlayersCommand(IPlayerAccountAdapter playerAccounterAdapter, IRepository<MasterPlayerAccountAggregate> repo)
+    {
+        _repository = repo;
+        _playerAccountAdapter = playerAccounterAdapter;
+    }
+
+    public async override Task<int> ExecuteAsync(CommandContext context, DeletePlayersCommandSettings settings)
+    {
+        var countItemsDeletedTask = await _repository.Get();
+        var countItemsDeleted = countItemsDeletedTask.Count;
+
+        if (settings.FromLocal.Length > 0)
+        {
+            await AnsiConsole.Progress().StartAsync(async ctx =>
+            {
+                var task = ctx.AddTask("[yellow]Deleting Users[/]", false);
+                task.StartTask();
+
+                var command = new DeletePlayersUseCase(_playerAccountAdapter, _repository);
+                await command.ExecuteAsync();
+
+                task.StopTask();
+            });
+
+            await _repository.Save(new List<MasterPlayerAccountAggregate>());
+        }
+        else //If FromLocal is not selected default to remote
+        {
+        }
+
+        AnsiConsole.MarkupLine("[bold green]All Users Deleted! Count: " + countItemsDeleted + "[/]");
+
+        return 0;
+    }
+    private static void HandleVerboseOutput(DeletePlayersBySegmentCommandSettings settings, List<MasterPlayerAccountAggregate> playersInSegment)
+    {
+        if (settings.Verbose && playersInSegment.Count > 0)
+        {
+            // Create a table
+            var table = new Table();
+
+            // Add some columns
+            table.AddColumn("MasterPlayer Account ID");
+
+            foreach (var account in playersInSegment)
+            {
+                table.AddRow(account.MasterPlayerAccount.Id ?? string.Empty);
+            }
+
+            // Render the table to the console
+            AnsiConsole.Write(table);
+        }
+    }
+}
+
---
+++ src/PlayFabBuddy.Cli/Commands/Player/DeletePlayersCommandSettings.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel;
+using Spectre.Console;
+using Spectre.Console.Cli;
+
+namespace PlayFabBuddy.Cli.Commands.Player;
+
+public class DeletePlayersCommandSettings : PlayerSettings
+{
+    [Description("LoadUsers to log in from local JSON File")]
+    [CommandOption("-l|--local")]
+    [CommandArgument(0, "[Local]")]
+    public string FromLocal { get; set; } = "";
+
+    [Description("LoadUsers to log in from PlayFab Segment")]
+    [CommandOption("-r|--remote")]
+    [CommandArgument(1, "[Remote]")]
+    public string FromRemote { get; set; } = "";
+
+    public override ValidationResult Validate()
+    {
+        if ((FromLocal.Length > 0 && FromRemote.Length == 0) || (FromRemote.Length > 0 && FromLocal.Length == 0))
+        {
+            return ValidationResult.Success();
+        }
+        else if (FromLocal.Length > 0 && FromRemote.Length > 0)
+        {
+            return ValidationResult.Error("Only one option can be set, eitehr FromLocal or FromRemote");
+        }
+        else
+        {
+            return ValidationResult.Error("And unexpected error occured in the command settings");
+        }
+    }
+}
