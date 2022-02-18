133e201ba637fa2dd2060df2554bbdd349db9aef
Annonator <andreaspohl89@gmail.com>
2022-02-18 15:09:47 +0100

    Made progress reporting on task list easier by handling if it is needed or not

--- src/PlayFabBuddy.Lib/UseCases/UseCase.cs
+++ src/PlayFabBuddy.Lib/UseCases/UseCase.cs
@@ -4,13 +4,16 @@ public abstract class UseCase<T> : IUseCase<T>
 {
     public abstract Task<T> ExecuteAsync(IProgress<double>? progress = null);
 
-    async protected Task ReportProgress(List<Task> tasks, IProgress<double> progress, double completed)
+    async protected Task ReportProgress(List<Task> tasks, IProgress<double>? progress, double completed)
     {
-        while (tasks.Any())
+        if (progress != null)
         {
-            var finishedTask = await Task.WhenAny(tasks);
-            tasks.Remove(finishedTask);
-            progress.Report(completed);
+            while (tasks.Any())
+            {
+                var finishedTask = await Task.WhenAny(tasks);
+                tasks.Remove(finishedTask);
+                progress.Report(completed);
+            }
         }
     }
 }
