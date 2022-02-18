15ff0f46a701de8fbea00e44b3760864e6aacc76
Annonator <andreaspohl89@gmail.com>
2022-02-18 15:06:44 +0100

    Added abstract UseCase to make progress implementation for usecases easier

---
+++ src/PlayFabBuddy.Lib/UseCases/UseCase.cs
@@ -0,0 +1,16 @@
+﻿namespace PlayFabBuddy.Lib.UseCases;
+
+public abstract class UseCase<T> : IUseCase<T>
+{
+    public abstract Task<T> ExecuteAsync(IProgress<double>? progress = null);
+
+    async protected Task ReportProgress(List<Task> tasks, IProgress<double> progress, double completed)
+    {
+        while (tasks.Any())
+        {
+            var finishedTask = await Task.WhenAny(tasks);
+            tasks.Remove(finishedTask);
+            progress.Report(completed);
+        }
+    }
+}
