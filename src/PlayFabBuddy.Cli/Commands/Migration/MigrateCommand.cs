d8c4dbdf71fd5316c97fe9105e11c214affb7342
Annonator <andreaspohl89@outlook.com>
2022-06-08 17:47:15 +0200

    Added initial migration command

---
+++ src/PlayFabBuddy.Cli/Commands/Migration/MigrateCommand.cs
@@ -0,0 +1,25 @@
+﻿using Spectre.Console;
+using Spectre.Console.Cli;
+
+namespace PlayFabBuddy.Cli.Commands.Migration;
+
+public class MigrateCommand : AsyncCommand<MigrateCommandSettings>
+{
+    public async override Task<int> ExecuteAsync(CommandContext context, MigrateCommandSettings settings)
+    {
+        await AnsiConsole.Progress().StartAsync(async ctx =>
+        {
+            var tileDataProgressTask = ctx.AddTask("[yellow]Copy Title Data[/]", false);
+            var tileInternalDataProgressTask = ctx.AddTask("[yellow]Copy Title Internal Data[/]", false);
+            var tileCurrencyProgressTask = ctx.AddTask("[yellow]Copy Currency[/]", false);
+            var tileFilesProgressTask = ctx.AddTask("[yellow]Copy Files[/]", false);
+            var tileCatalogsProgressTask = ctx.AddTask("[yellow]Copy Catalogs[/]", false);
+            var tileStoresProgressTask = ctx.AddTask("[yellow]Copy Stores[/]", false);
+            var tileLegacyCloudScriptsProgressTask = ctx.AddTask("[yellow]Copy (Legacy) Cloud Scripts[/]", false);
+
+
+        });
+
+        return 0;
+    }
+}
---
+++ src/PlayFabBuddy.Cli/Commands/Migration/MigrateCommandSettings.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel;
+using Spectre.Console;
+using Spectre.Console.Cli;
+
+namespace PlayFabBuddy.Cli.Commands.Migration;
+
+public class MigrateCommandSettings : CommandSettings
+{
+    [Description("The Title Id you want to copy from")]
+    [CommandArgument(0, "<SourceTitleId>")]
+    public string SourceTitleId { get; set; } = "";
+
+    [Description("The Title Id you want to copy to")]
+    [CommandArgument(1, "<DestinationTitleId>")]
+    public string DestinationTitleId { get; set; } = "";
+
+    public override ValidationResult Validate()
+    {
+        if (SourceTitleId.Length > 0 && DestinationTitleId.Length > 0)
+        {
+            return ValidationResult.Success();
+        }
+
+        return ValidationResult.Error("And unexpected error occured in the command settings");
+    }
+}
