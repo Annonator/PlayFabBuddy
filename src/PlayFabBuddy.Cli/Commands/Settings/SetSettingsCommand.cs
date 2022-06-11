a5a35cc8d9053153ecc086223b780bf2aea84daf
Annonator <andreaspohl89@outlook.com>
2022-06-11 19:38:36 +0200

    Resolves #83 by giving the option to change config and display currently used config

---
+++ src/PlayFabBuddy.Cli/Commands/Settings/SetSettingsCommand.cs
@@ -0,0 +1,51 @@
+﻿using System.Text.Json;
+using Microsoft.Extensions.Configuration;
+using Spectre.Console;
+using Spectre.Console.Cli;
+
+namespace PlayFabBuddy.Cli.Commands.Settings;
+
+public class SetSettingsCommand : AsyncCommand<SetSettingsSettings>
+{
+    private readonly IConfiguration _config;
+
+    public SetSettingsCommand(IConfiguration config)
+    {
+        _config = config;
+    }
+
+    public override Task<int> ExecuteAsync(CommandContext context, SetSettingsSettings settings)
+    {
+        if (!settings.WantToOverwrite)
+        {
+            if (!AnsiConsole.Confirm("This will overwrite any existing local settings, want to continue?"))
+            {
+                return Task.FromResult(0);
+            }
+        }
+
+        if (settings.TitleId.Length == 0)
+        {
+            settings.TitleId = AnsiConsole.Ask<string>("Whats the Title Id you want PlayFabBuddy to use?");
+        }
+
+        if (settings.DevSecret.Length == 0)
+        {
+            settings.DevSecret = AnsiConsole.Prompt(
+                new TextPrompt<string>("Whats the Dev Secret for this title?").Secret()
+            );
+        }
+
+        _config["titleId"] = settings.TitleId;
+        _config["devSecret"] = settings.DevSecret;
+        _config["defaultSavePath"] = settings.MasterAccountDefaultSavePath;
+
+        // TODO: Move this into a repo, as we are overlapping concerns here. 
+        var configAsDictionary = _config.AsEnumerable().ToDictionary(c => c.Key, c => c.Value);
+        var options = new JsonSerializerOptions { WriteIndented = true };
+        var json = JsonSerializer.Serialize(configAsDictionary, options);
+        File.WriteAllText("local.settings.json", json);
+
+        return Task.FromResult(0);
+    }
+}
---
+++ src/PlayFabBuddy.Cli/Commands/Settings/SetSettingsSettings.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel;
+using Spectre.Console.Cli;
+
+namespace PlayFabBuddy.Cli.Commands.Settings;
+
+public class SetSettingsSettings : SettingsSettings
+{
+    [Description("The Title ID used by default in any commands a source.")]
+    [CommandOption("-o|--overwrite")]
+    public bool WantToOverwrite { get; set; } = false;
+
+    [Description("The Title ID used by default in any commands a source.")]
+    [CommandOption("-i|--id")]
+    public string TitleId { get; set; } = "";
+
+    [Description("The DevSecret for the main Title Id configured")]
+    [CommandOption("-s|--secret")]
+    public string DevSecret { get; set; } = "";
+
+    [Description("The default path to save the local Master Account config file to be used with PF Buddy")]
+    [CommandOption("-m|--masterdefaultpath")]
+    public string MasterAccountDefaultSavePath { get; set; } = "MasterAccountOutput.json";
+}
