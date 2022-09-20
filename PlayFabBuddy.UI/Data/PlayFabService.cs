896a5419011b00f4260791445d9c448ba542d3ac
Dean <deanhume@gmail.com>
2022-09-20 14:52:01 +0100

    Added the UI Project

---
+++ PlayFabBuddy.UI/Data/PlayFabService.cs
@@ -0,0 +1,32 @@
+﻿using PlayFabBuddy.Lib.Aggregate;
+using PlayFabBuddy.Lib.Interfaces.Adapter;
+using PlayFabBuddy.Lib.UseCases.Player;
+using System.Net;
+
+namespace PlayFabBuddy.UI.Data
+{
+    public class PlayFabService
+    {
+        private readonly IDataExplorerAdapter _dataAdapter;
+        private readonly IPlayerAccountAdapter _playerAdapter;
+
+        public PlayFabService(IDataExplorerAdapter dataAdapter, IPlayerAccountAdapter playerAccountAdapter)
+        {
+            _dataAdapter = dataAdapter;
+            _playerAdapter = playerAccountAdapter;
+        }
+
+        public Task<WeatherForecast[]> GetForecastAsync()
+        {
+            string ipAddress = "123.123.123.0";
+
+            var getPlayerUsecase = new GetPlayersByIpUseCAse(_dataAdapter, _playerAdapter, IPAddress.Parse(ipAddress));
+
+            return Task.FromResult(Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+            }).ToArray());
+        }
+    }
+}
---
+++ PlayFabBuddy.UI/Program.cs
@@ -0,0 +1,45 @@
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.Extensions.DependencyInjection;
+using PlayFabBuddy.Infrastructure;
+using PlayFabBuddy.Infrastructure.Config;
+using PlayFabBuddy.Lib;
+using PlayFabBuddy.UI.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var customConfigBuilder = new ConfigurationBuilder();
+customConfigBuilder.AddJsonFile("settings.json", false, true);
+customConfigBuilder.AddJsonFile("local.settings.json", true, true);
+
+var customConfig = customConfigBuilder.Build();
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddSingleton<IConfiguration>(customConfig);
+builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddSingleton<PlayFabService>();
+builder.Services.AddLibrary(customConfig);
+builder.Services.AddInfrastructure(customConfig);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
